8daf66bf07fad741b3984366219b27282ca1b215
Duncan McGreggor <oubiwann@gmail.com>
2016-11-01 11:12:48 -0500

    Added more make targets.

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
-docs:
-	lein with-profile docs codox
+VERSION=0.5.0
+PROJECT=meson
+STANDALONE=target/$(PROJECT)-$(VERSION)-SNAPSHOT-standalone.jar
+ROOT_DIR = $(shell pwd)
+
+include dev-resources/make/docs.mk
+include dev-resources/make/test.mk
---
+++ dev-resources/make/test.mk
@@ -0,0 +1,41 @@
+kibit:
+	@lein with-profile +test kibit
+
+eastwood:
+	@lein with-profile +test eastwood \
+	"{:namespaces [:source-paths]}"
+
+lint: kibit eastwood
+
+lint-unused:
+	@lein with-profile +test eastwood \
+	"{:linters [:unused-fn-args \
+	            :unused-locals \
+	            :unused-namespaces \
+	            :unused-private-vars \
+	            :wrong-ns-form] \
+	  :namespaces [:source-paths]}"
+
+lint-ns:
+	@lein with-profile +test eastwood \
+	"{:linters [:unused-namespaces :wrong-ns-form] \
+	  :namespaces [:source-paths]}"
+
+unit:
+	@lein with-profile +test test :unit
+
+integration:
+	@lein with-profile +test test :integration
+
+system:
+	@lein with-profile +test test :system
+
+all-tests:
+	@lein with-profile +test test :all
+
+check: kibit all-tests
+
+check-all: lint all-tests
+
+check-no-lint:
+	@lein with-profile +test,-dev test :all
